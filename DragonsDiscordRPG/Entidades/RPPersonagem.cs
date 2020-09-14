29089bef3e5fc16fb0683f8daf907e8dfa463c40
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-09-14 13:23:29 -0300

    Limitar exibição de regen para 2 casas decimais

--- DragonsDiscordRPG/Entidades/RPPersonagem.cs
+++ DragonsDiscordRPG/Entidades/RPPersonagem.cs
@@ -158,11 +158,11 @@ namespace DragonsDiscordRPG.Entidades
                         if (regen) continue;
                         regen = true;
                         Vida.Adicionar(Efeitos[i].Quantidade);
-                        ataquesInimigos.AppendLine($"{Efeitos[i].Quantidade} de vida regenerado.");
+                        ataquesInimigos.AppendLine($"{Efeitos[i].Quantidade.Text()} de vida regenerado.");
                         if (Efeitos[i].Usar())
                         {
                             Efeitos.RemoveAt(i);
-                            ataquesInimigos.AppendLine($"Regen de vida acabou.");
+                            ataquesInimigos.AppendLine($"Regen de vida acabou.".Bold());
                         }
                         break;
                 }
