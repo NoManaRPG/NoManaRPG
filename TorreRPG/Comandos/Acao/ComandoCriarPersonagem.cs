41d8ad89c77537760b0f2afd2e69997932557579
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-09-19 21:41:37 -0300

    Adicionar itens iniciais para os personagens

--- TorreRPG/Comandos/Acao/ComandoCriarPersonagem.cs
+++ TorreRPG/Comandos/Acao/ComandoCriarPersonagem.cs
@@ -45,52 +45,56 @@ namespace TorreRPG.Comandos.Acao
                 return;
             }
 
-            string classeFormatada = RemoveDiacritics(classe).ToLower();
+            string classeFormatada = classe.RemoverAcentos().ToLower();
 
             switch (classeFormatada)
             {
                 case "cacadora":
-                    await CriarJogador(ctx, new RPPersonagem("Caçadora", nomePersonagem, new RPAtributo(14, 32, 14), new RPDano(2, 5)));
+                    var per1 = new RPPersonagem("Caçadora", nomePersonagem, new RPAtributo(14, 32, 14), new RPDano(2, 5));
+                    per1.Mochila.TryAddItem(new Metadata.Itens.Armas.DuasMaoArmas.Arcos().Arco1());
+                    per1.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per1);
                     break;
                 case "berserker":
-                    await CriarJogador(ctx, new RPPersonagem("Berserker", nomePersonagem, new RPAtributo(14, 14, 32), new RPDano(2, 8)));
-                    break;
-                case "bruxa":
-                    await CriarJogador(ctx, new RPPersonagem("Bruxa", nomePersonagem, new RPAtributo(32, 14, 14), new RPDano(2, 5)));
-                    break;
-                case "duelista":
-                    await CriarJogador(ctx, new RPPersonagem("Duelista", nomePersonagem, new RPAtributo(14, 23, 23), new RPDano(2, 6)));
-                    break;
-                case "templario":
-                    await CriarJogador(ctx, new RPPersonagem("Templário", nomePersonagem, new RPAtributo(23, 14, 23), new RPDano(2, 6)));
+                    var per2 = new RPPersonagem("Berserker", nomePersonagem, new RPAtributo(14, 14, 32), new RPDano(2, 8));
+                    per2.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.MacasUmaMao().Maca1());
+                    per2.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per2);
                     break;
                 case "sombra":
-                    await CriarJogador(ctx, new RPPersonagem("Sombra", nomePersonagem, new RPAtributo(23, 23, 14), new RPDano(2, 5)));
+                    var per6 = new RPPersonagem("Sombra", nomePersonagem, new RPAtributo(23, 23, 14), new RPDano(2, 5));
+                    per6.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Adagas().Adaga1());
+                    per6.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per6);
+                    break;
+                case "duelista":
+                    var per4 = new RPPersonagem("Duelista", nomePersonagem, new RPAtributo(14, 23, 23), new RPDano(2, 6));
+                    per4.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.EspadasUmaMao().Espada1());
+                    per4.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per4);
+                    break;
+                case "bruxa":
+                    var per3 = new RPPersonagem("Bruxa", nomePersonagem, new RPAtributo(32, 14, 14), new RPDano(2, 5));
+                    per3.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Varinhas().Varinha1());
+                    per3.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per3);
+                    break;
+                case "templario":
+                    var per5 = new RPPersonagem("Templário", nomePersonagem, new RPAtributo(23, 14, 23), new RPDano(2, 6));
+                    per5.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Cetros().Cetro1());
+                    per5.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per5);
                     break;
                 case "herdeira":
-                    await CriarJogador(ctx, new RPPersonagem("Herdeira", nomePersonagem, new RPAtributo(20, 20, 20), new RPDano(2, 6)));
+                    var per7 = new RPPersonagem("Herdeira", nomePersonagem, new RPAtributo(20, 20, 20), new RPDano(2, 6));
+                    per7.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.EspadasUmaMao().Espada1());
+                    per7.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
+                    await CriarJogador(ctx, per7);
                     break;
                 default:
                     await ctx.RespondAsync($"{ctx.User.Mention}, você informou uma classe que não existe!");
                     break;
             }
-        }
-
-        static string RemoveDiacritics(string text)
-        {
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder();
-
-            foreach (var c in normalizedString)
-            {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
-            }
-
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
 
         private async Task<bool> JogadorExisteAsync(CommandContext ctx)
