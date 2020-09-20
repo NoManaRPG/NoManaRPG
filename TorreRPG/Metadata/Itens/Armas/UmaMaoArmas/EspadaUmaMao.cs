3a3f979c87208f4eb1f57c4a944d4fd7c6e5b0cf
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-09-19 21:20:15 -0300

    Renomear nome espada

--- TorreRPG/Metadata/Itens/Armas/UmaMaoArmas/EspadaUmaMao.cs
+++ TorreRPG/Metadata/Itens/Armas/UmaMaoArmas/EspadaUmaMao.cs
@@ -10,23 +10,23 @@ namespace TorreRPG.Metadata.Itens.Armas.UmaMaoArmas
         public List<RPEspada> EspadaAb()
         {
             var arma = new List<RPEspada>();
-            arma.Add(Arma1());
-            arma.Add(Arma2());
-            arma.Add(Arma3());
-            arma.Add(Arma4());
+            arma.Add(Espada1());
+            arma.Add(Espada2());
+            arma.Add(Espada3());
+            arma.Add(Espada4());
             return arma;
         }
 
-        public RPEspada Arma1()
+        public RPEspada Espada1()
         => new RPEspada(1, "Espada Enferrujada", RPClasse.UmaMaoArma, 3, new RPDano(4, 9), 0.05, 1.55, 8, 8);
 
-        public RPEspada Arma2()
+        public RPEspada Espada2()
         => new RPEspada(5, "Espada de Cobre", RPClasse.UmaMaoArma, 3, new RPDano(6, 14), 0.05, 1.5, 14, 14);
 
-        public RPEspada Arma3()
+        public RPEspada Espada3()
         => new RPEspada(10, "Sabre", RPClasse.UmaMaoArma, 3, new RPDano(5, 22), 0.05, 1.55, 18, 26);
 
-        public RPEspada Arma4()
+        public RPEspada Espada4()
         => new RPEspada(15, "Espada Larga", RPClasse.UmaMaoArma, 3, new RPDano(15, 21), 0.05, 1.45, 30, 30);
     }
 }
