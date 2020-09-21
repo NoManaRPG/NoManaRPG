9d8a4235d54efd1ca98656d6c527b8decceab6f5
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-09-21 10:01:42 -0300

    Dar cargo ao escolher a classe

--- TorreRPG/Comandos/Acao/ComandoCriarPersonagem.cs
+++ TorreRPG/Comandos/Acao/ComandoCriarPersonagem.cs
@@ -54,42 +54,49 @@ namespace TorreRPG.Comandos.Acao
                     per1.Mochila.TryAddItem(new Metadata.Itens.Armas.DuasMaoArmas.Arcos().Arco1());
                     per1.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per1);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575940216979562));
                     break;
                 case "berserker":
                     var per2 = new RPPersonagem("Berserker", nomePersonagem, new RPAtributo(14, 14, 32), new RPDano(2, 8));
                     per2.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.MacasUmaMao().Maca1());
                     per2.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per2);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575413697478738));
                     break;
                 case "sombra":
                     var per6 = new RPPersonagem("Sombra", nomePersonagem, new RPAtributo(23, 23, 14), new RPDano(2, 5));
                     per6.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Adagas().Adaga1());
                     per6.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per6);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575488762806392));
                     break;
                 case "duelista":
                     var per4 = new RPPersonagem("Duelista", nomePersonagem, new RPAtributo(14, 23, 23), new RPDano(2, 6));
                     per4.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.EspadasUmaMao().Espada1());
                     per4.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per4);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575879420411944));
                     break;
                 case "bruxa":
                     var per3 = new RPPersonagem("Bruxa", nomePersonagem, new RPAtributo(32, 14, 14), new RPDano(2, 5));
                     per3.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Varinhas().Varinha1());
                     per3.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per3);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575261448306701));
                     break;
                 case "templario":
                     var per5 = new RPPersonagem("Templário", nomePersonagem, new RPAtributo(23, 14, 23), new RPDano(2, 6));
                     per5.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.Cetros().Cetro1());
                     per5.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per5);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575545516195900));
                     break;
                 case "herdeira":
                     var per7 = new RPPersonagem("Herdeira", nomePersonagem, new RPAtributo(20, 20, 20), new RPDano(2, 6));
                     per7.Mochila.TryAddItem(new Metadata.Itens.Armas.UmaMaoArmas.EspadasUmaMao().Espada1());
                     per7.Mochila.TryAddItem(new Metadata.Itens.Frascos.FrascosVida().Frasco1());
                     await CriarJogador(ctx, per7);
+                    await ctx.Member.GrantRoleAsync(ctx.Guild.GetRole(757575351789420546));
                     break;
                 default:
                     await ctx.RespondAsync($"{ctx.User.Mention}, você informou uma classe que não existe!");
