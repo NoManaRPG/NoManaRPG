9e30d6f7f8005df63aeaf00a6466b124fb7e3ed6
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-10-23 10:51:33 -0300

    Mudar posição das moedas com base no nível

--- TorreRPG/Metadata/Itens/MoedasEmpilhaveis/MoedasEmpilhaveis.cs
+++ TorreRPG/Metadata/Itens/MoedasEmpilhaveis/MoedasEmpilhaveis.cs
@@ -14,10 +14,10 @@ namespace TorreRPG.Metadata.Itens.MoedasEmpilhaveis
             return pergaminho;
         }
 
-        public RPMoedaEmpilhavel PergaminhoSabedoria()
-        => new RPMoedaEmpilhavel(1, "Pergaminho de Sabedoria", RPClasse.PergaminhoSabedoria, 1, 40);
         public RPMoedaEmpilhavel PergaminhoFragmento()
         => new RPMoedaEmpilhavel(0, "Fragmento de Pergaminho", RPClasse.FragmentoPergaminho, 1, 5);
+        public RPMoedaEmpilhavel PergaminhoSabedoria()
+        => new RPMoedaEmpilhavel(1, "Pergaminho de Sabedoria", RPClasse.PergaminhoSabedoria, 1, 40);
         public RPMoedaEmpilhavel PergaminhoPortal()
         => new RPMoedaEmpilhavel(4, "Pergaminho de Portal", RPClasse.PergaminhoPortal, 1, 40);
     }
