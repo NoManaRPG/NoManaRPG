7c4cc2ca95f0d985988bf99f8f253a7fd192ee7f
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-11-10 13:02:36 -0300

    Change emoji coins

--- WafclastRPG.Game/Emoji.cs
+++ WafclastRPG.Game/Emoji.cs
@@ -28,7 +28,7 @@
         public const string Fome = ":poultry_leg:";
         public const string Sede = ":cup_with_straw:";
 
-        public const string Coins = "<:coins:774232103864107038>";
+        public const string Coins = "<a:coins:775750607738896436>";
         public const string Exp = "<:xp:758439721016885308>";
     }
 }
