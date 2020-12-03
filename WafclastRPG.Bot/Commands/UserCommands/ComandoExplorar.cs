0808528e35a62af9a4a9bfd84cd16f72dd645206
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-12-03 11:24:15 -0300

    Remove option to leave

--- WafclastRPG.Bot/Commands/UserCommands/ComandoExplorar.cs
+++ WafclastRPG.Bot/Commands/UserCommands/ComandoExplorar.cs
@@ -23,43 +23,36 @@ namespace WafclastRPG.Bot.Comandos.Acao
 
         [Command("explorar")]
         [Aliases("ex")]
-        [Description("Você explora uma região atrás de aventuras.")]
-        [Example("explorar 1", "Explora a região por novos inimigos.")]
-        [Example("explorar", "Explora a região e ataca os inimigos presentes.")]
-        [Usage("explorar [ tipo ]")]
-        [Cooldown(1, 4, CooldownBucketType.User)]
-        public async Task ComandoExplorarAsync(CommandContext ctx, string codigo = "4")
+        [Description("Você explora uma região atacando monstros que vê no caminho.")]
+        [Example("explorar 2", "Você ataca um monstro por 1.2 segundos .")]
+        [Example("explorar", "Você ataca um monstro por 2.4 segundos")]
+        [Usage("explorar [ ticks ]")]
+        [Cooldown(1, 6, CooldownBucketType.User)]
+        public async Task ComandoExplorarAsync(CommandContext ctx, string stringTicks = "4")
         {
             /*
-             * Procura por inimigos caso não tenha um.
-             * Caso tenha inimigos, o ataca por x ticks
-             * Ao abater o inimigo, ele não some.
-             * O jogador tem a opção de usar o comando "Saquear".
-             * Saquear é um comando interativo, onde pergunta quais itens ele deseja pegar.
-             * 
+             * Monstro e Jogador batalham entre sí por x(ticks) tempo.
+             * Caso o monstro já tenha morrido, faz aparecer outro.
+             * Caso o jogador morra, ele perde todos os itens da mochila e equipados, voltando assim para a região 0.
+             * Caso o monstro morra, ele continua no chão para ser usado no comando saquear.
              */
 
             // Trava o jogador de inicializar outro comando.
             using (await banco.LockAsync(ctx.User.Id))
             {
-                bool fugir = false;
-                if (!int.TryParse(codigo, out int tempo))
+                if (!int.TryParse(stringTicks, out int ticks))
                 {
-                    if (codigo.ToLower() == "fugir")
-                        fugir = true;
-                    else
-                    {
-                        await ctx.RespondAsync($"{ctx.User.Mention}, argumentos inválidos.");
-                        return;
-                    }
+                    await ctx.RespondAsync($"{ctx.User.Mention}, ticks informado é inválido.");
+                    return;
                 }
-                tempo = Math.Clamp(tempo, 1, 10);
+                // Limitamos para não ultrapassar o limite de caracteres do Discord.
+                ticks = Math.Clamp(ticks, 2, 20);
 
                 var jogador = await banco.GetJogadorAsync(ctx);
                 var per = jogador.Personagem;
 
                 // Sempre precisa ter inimigos para batalhar.
-                if (fugir || per.InimigoMonstro == null || per.InimigoMonstro.Vida <= 0)
+                if (per.InimigoMonstro == null || per.InimigoMonstro.Vida <= 0)
                 {
                     var reg = await jogador.GetRegiaoAsync();
                     per.InimigoMonstro = rdg.Sortear(reg.Monstros);
@@ -67,7 +60,7 @@ namespace WafclastRPG.Bot.Comandos.Acao
 
                 var batalha = new StringBuilder();
                 DiscordEmbedBuilder embed = new DiscordEmbedBuilder().Inicializar(ctx.User);
-                embed.WithDescription($"Ataque durou {Emoji.Relogio} **{(int.Parse(codigo) * 0.6):N0}s**");
+                embed.WithDescription($"Ataque durou {Emoji.Relogio} **{(ticks * 0.6):N0}s**");
 
                 // Ataques
                 var habAtaque = per.GetHabilidade(ProficienciaType.Ataque) as WafclastProficienciaAtaque;
@@ -76,7 +69,7 @@ namespace WafclastRPG.Bot.Comandos.Acao
                 WafclastItemArma arma2 = null;
                 var chance = habAtaque.ChanceAcerto(habAtaque.Precisao, per.InimigoMonstro.GetDefesa());
                 bool temArma = false;
-                while (tempo > 0)
+                while (ticks > 0)
                 {
                     if (per.TryGetEquipamento(EquipamentoType.PrimeiraMao, out var item))
                     {
@@ -98,7 +91,7 @@ namespace WafclastRPG.Bot.Comandos.Acao
                     }
                     if (!temArma)
                     {
-                        await ctx.RespondAsync($"{ctx.User.Mention}, é necessário estar equipado com uma arma para explorar");
+                        await ctx.RespondAsync($"{ctx.User.Mention}, é necessário estar equipado com uma arma para explorar.");
                         return;
                     }
 
@@ -134,7 +127,7 @@ namespace WafclastRPG.Bot.Comandos.Acao
                         }
                         batalha.AppendLine($"{Emoji.Mago} **{ctx.User.Mention} defendeu!** {Emoji.Escudo}");
                     }
-                    tempo--;
+                    ticks--;
                 }
 
 
