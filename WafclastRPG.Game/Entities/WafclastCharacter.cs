c7808dde19f8a6fb3093606cb6907c207f7ffb58
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-02-16 16:43:26 -0300

    First Character

--- WafclastRPG.Game/Entities/WafclastCharacter.cs
+++ WafclastRPG.Game/Entities/WafclastCharacter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,30 @@ namespace WafclastRPG.Game.Entities
 {
     public class WafclastCharacter
     {
+        public int Level { get; private set; }
+        public WafclastCoins Coins { get; private set; }
+
+        public WafclastCharacter()
+        {
+            Level = 0;
+        }
+
+        public void AddLevel()
+        {
+            Level++;
+        }
+        public void RemoveLevel()
+        {
+            Level--;
+        }
+
+        public static void MapBuilder()
+        {
+            BsonClassMap.RegisterClassMap<WafclastCharacter>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+        }
     }
 }
