8ae30c1fe0032aaac77284b55249ac8b1c61dcac
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-03-03 16:32:19 -0300

    Wafclast Monster

---
+++ WafclastRPG.Game/Entities/WafclastMonster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WafclastRPG.Game.Entities
+{
+    public class WafclastMonster
+    {
+        public ulong Id { get; private set; }
+        public ulong MonsterId { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Defesa { get; private set; } = 0;
+        public decimal Ataque { get; private set; } = 0;
+        public decimal Vida { get; private set; } = 0;
+
+        public DateTime DateSpawn { get; private set; }
+
+        public WafclastMonster(ulong id, ulong monsterId)
+        {
+            Id = id + monsterId;
+            MonsterId = monsterId;
+        }
+    }
+}
---
+++ WafclastRPG.Game/Entities/WafclastMonsterAtributos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WafclastRPG.Game.Entities
+{
+    public class WafclastMonsterAtributos
+    {
+        /// <summary>
+        /// Aumenta o dano físico máximo em 2 por cada ponto.
+        /// </summary>
+        public int Forca { get; private set; }
+        /// <summary>
+        /// Aumenta a vida máxima em 4 por cada ponto.
+        /// </summary>
+        public int Resistencia { get; private set; }
+        /// <summary>
+        /// Aumenta a velocidade de recuperação de vida por mensagem. Cada ponto recupera 0.2 de vida.
+        /// </summary>
+        public int Defesa { get; private set; }
+    }
+}
