3952dd71a93abb36bdf35c59163d3f193a760019
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-04-16 11:32:25 -0300

    moved database methods to player

--- WafclastRPG/DataBases/DatabaseSession.cs
+++ WafclastRPG/DataBases/DatabaseSession.cs
@@ -14,7 +14,7 @@ namespace WafclastRPG.DataBases
     public class DatabaseSession : IDisposable
     {
         public IClientSessionHandle Session { get; }
-        private DataBase Database { get; }
+        public DataBase Database { get; }
 
         public DatabaseSession(IClientSessionHandle session, DataBase database)
         {
@@ -24,24 +24,19 @@ namespace WafclastRPG.DataBases
 
         public Task<Response> WithTransactionAsync(Func<IClientSessionHandle, CancellationToken, Task<Response>> callbackAsync) => Session.WithTransactionAsync(callbackAsync: callbackAsync);
 
-        public void Dispose() => Session.Dispose();
-
-        #region FindAsync
 
 
-        public Task<WafclastPlayer> FindAsync(DiscordUser user)
-          => Database.CollectionPlayers.Find(Session, x => x.Id == user.Id).FirstOrDefaultAsync();
-        public Task<WafclastMonster> FindMonsterAsync(int floorLevel)
-          => Database.CollectionMonsters.AsQueryable().Sample(1).Where(x => x.FloorLevel <= floorLevel).FirstOrDefaultAsync();
+
+        public async Task<WafclastPlayer> FindAsync(DiscordUser user)
+        {
+            var player = await Database.CollectionPlayers.Find(Session, x => x.Id == user.Id).FirstOrDefaultAsync();
+            player.Session = this;
+            return player;
+        }
+
         public Task<WafclastBaseItem> FindItemAsync(ObjectId id)
          => Database.CollectionItems.Find(Session, x => x.Id == id).FirstOrDefaultAsync();
-        public Task<WafclastBaseItem> FindAsync(string itemName, WafclastPlayer player)
-                => Database.CollectionItems.Find(Session, x => x.PlayerId == player.Id && x.Name == itemName,
-                    new FindOptions { Collation = new Collation("pt", false, strength: CollationStrength.Primary) }).FirstOrDefaultAsync();
 
-
-        #endregion
-        #region Replace
 
 
         public Task ReplaceAsync(WafclastPlayer jogador)
@@ -54,48 +49,13 @@ namespace WafclastRPG.DataBases
            => Database.CollectionMonsters.ReplaceOneAsync(x => x.Id == monster.Id, monster, new ReplaceOptions { IsUpsert = true });
 
 
-        #endregion
-        #region Insert
-
-
-        public async Task InsertAsync(WafclastBaseItem item)
-        {
-            item.Id = ObjectId.Empty;
-            await Database.CollectionItems.InsertOneAsync(Session, item);
-        }
-        public async Task InsertAsync(WafclastBaseItem item, int quantity, WafclastPlayer player)
-        {
-            item.PlayerId = player.Id;
-            item.Quantity = 1;
-            if (!item.CanStack)
-            {
-                for (int i = 0; i < quantity; i++)
-                    await InsertAsync(item);
-                return;
-            }
-
-            var itemFound = await FindAsync(item.Name, player);
-            if (itemFound == null)
-            {
-                item.Quantity = quantity;
-                await InsertAsync(item);
-                return;
-            }
-
-            itemFound.Quantity += quantity;
-            await ReplaceAsync(itemFound);
-        }
-
-
-        #endregion
-        #region Remove
-
+        public Task InsertAsync(WafclastBaseItem item)
+            => Database.CollectionItems.InsertOneAsync(Session, item);
 
         public Task RemoveAsync(WafclastBaseItem item)
            => Database.CollectionItems.DeleteOneAsync(Session, x => x.Id == item.Id);
 
-
-        #endregion
+        public void Dispose() => Session.Dispose();
     }
 
     public class Response
--- WafclastRPG/Entities/WafclastPlayer.cs
+++ WafclastRPG/Entities/WafclastPlayer.cs
@@ -1,11 +1,20 @@
-﻿using MongoDB.Bson.Serialization;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using System;
 using System.Globalization;
+using System.Threading.Tasks;
+using WafclastRPG.DataBases;
+using WafclastRPG.Entities.Itens;
+using WafclastRPG.Entities.Monsters;
 
 namespace WafclastRPG.Entities
 {
+    [BsonIgnoreExtraElements]
     public class WafclastPlayer
     {
+        [BsonId]
         public ulong Id { get; private set; }
         public DateTime DateAccountCreation { get; private set; }
         public WafclastCharacter Character { get; private set; }
@@ -25,14 +34,56 @@ namespace WafclastRPG.Entities
 
         public string Mention { get => $"<@{Id.ToString(CultureInfo.InvariantCulture)}>"; }
 
-        public static void MapBuilder()
+        [BsonIgnore]
+        public DatabaseSession Session { get; set; }
+
+        /// <summary>
+        /// Somente usar em uma sessão.
+        /// <para>Retorna um monstro aleatorio baseado no andar do personagem.</para>
+        /// </summary>
+        /// <returns></returns>
+        public Task<WafclastMonster> GetNewMonsterAsync()
+          => Session.Database.CollectionMonsters.AsQueryable().Sample(1).Where(x => x.FloorLevel <= Character.CurrentFloor).FirstOrDefaultAsync();
+
+        /// <summary>
+        /// Somente usar em uma sessão.
+        /// <para>Retorna um item que está no inventário do personagem.</para>
+        /// </summary>
+        /// <param name="name">Nome do Item</param>
+        /// <returns>Item</returns>
+        public Task<WafclastBaseItem> GetItemAsync(string name)
+           => Session.Database.CollectionItems.Find(Session.Session, x => x.PlayerId == Id && x.Name == name,
+                 new FindOptions { Collation = new Collation("pt", false, strength: CollationStrength.Primary) }).FirstOrDefaultAsync();
+
+        /// <summary>
+        /// Somente usar em uma sessão.
+        /// <para>Adiciona um item no inventário do personagem.</para>
+        /// </summary>
+        /// <param name="item">Item a ser adicionado.</param>
+        /// <param name="quantity">Quantidade a ser adicionada.</param>
+        /// <returns></returns>
+        public async Task AddItemAsync(WafclastBaseItem item, int quantity)
         {
-            BsonClassMap.RegisterClassMap<WafclastPlayer>(cm =>
+            var foundItem = await GetItemAsync(item.Name);
+            if (foundItem == null)
             {
-                cm.AutoMap();
-                cm.SetIgnoreExtraElements(true);
-                cm.MapIdMember(c => c.Id);
-            });
+                item.Id = ObjectId.GenerateNewId();
+                item.PlayerId = Id;
+                item.Quantity = quantity;
+                await Session.InsertAsync(item);
+                return;
+            }
+
+            foundItem.Quantity += quantity;
+            await Session.ReplaceAsync(foundItem);
         }
+
+        /// <summary>
+        /// Somente usar em uma sessão.
+        /// <para>Salva o personagem.</para>
+        /// </summary>
+        /// <returns></returns>
+        public Task SaveAsync()
+            => Session.Database.CollectionPlayers.ReplaceOneAsync(Session.Session, x => x.Id == Id, this, new ReplaceOptions { IsUpsert = true });
     }
 }
