ab30283f5fcaaa653b5506c0caedd7d8c1f54004
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-04-19 10:48:27 -0300

    changed format of coins to one type

--- WafclastRPG/Entities/WafclastCoins.cs
+++ WafclastRPG/Entities/WafclastCoins.cs
@@ -1,60 +1,20 @@
-﻿using MongoDB.Bson.Serialization;
-using System;
-using System.Text;
-
-namespace WafclastRPG.Entities
+﻿namespace WafclastRPG.Entities
 {
     public class WafclastCoins
     {
-        private const ulong SilverInCopper = 100;
-        private const ulong GoldInCopper = SilverInCopper * 100;
-        private const ulong PlatinumInCopper = GoldInCopper * 100;
-
         public ulong Coins { get; set; } = 0;
 
-        public WafclastCoins() { }
         public WafclastCoins(ulong startCoins) => Coins = startCoins;
 
-        public void Add(ulong gold, ulong silver, ulong copper)
-        {
-            Coins += gold * GoldInCopper;
-            Coins += silver * SilverInCopper;
-            Coins += copper;
-        }
+        public void Add(ulong quantidade)
+            => Coins += quantidade;
 
-        public void Subtract(int coins) => Coins -= Convert.ToUInt64(coins);
+        public void Subtract(ulong quantidade)
+            => Coins -= quantidade;
 
         public override string ToString()
         {
-            var str = new StringBuilder();
-            var ouro = GetGold();
-            if (ouro != 0)
-                str.Append($"{ouro} ouro, ");
-
-            var prata = GetSilver();
-            if (prata != 0)
-                str.Append($"{prata} prata e ");
-
-            str.Append($"{GetCopper()} cobre.");
-            return str.ToString();
+            return Coins.ToString();
         }
-
-        public ulong GetCopper() => Coins % SilverInCopper;
-        public ulong GetSilver() => Coins % GoldInCopper / SilverInCopper;
-        public ulong GetGold() => Coins / GoldInCopper;
-
-        #region Static Methods
-        public static ulong GetCopper(ulong baseDenomination) => baseDenomination % SilverInCopper;
-        public static ulong GetSilver(ulong baseDenomination) => baseDenomination % GoldInCopper / SilverInCopper;
-        public static ulong GetGold(ulong baseDenomination) => baseDenomination / GoldInCopper;
-        public static void MapBuilder()
-        {
-            BsonClassMap.RegisterClassMap<WafclastCoins>(cm =>
-            {
-                cm.AutoMap();
-                cm.SetIgnoreExtraElements(true);
-            });
-        }
-        #endregion
     }
 }
