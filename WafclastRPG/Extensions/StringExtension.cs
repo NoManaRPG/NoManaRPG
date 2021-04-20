a04d8416b2ddefa546535a72ca61288112f21edc
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-04-20 10:54:56 -0300

    removed unused methods

--- WafclastRPG/Extensions/StringExtension.cs
+++ WafclastRPG/Extensions/StringExtension.cs
@@ -1,7 +1,6 @@
 ﻿using DSharpPlus;
 using System;
 using System.Globalization;
-using System.Linq;
 using System.Text;
 
 namespace WafclastRPG.Extensions
@@ -24,23 +23,11 @@ namespace WafclastRPG.Extensions
 
             return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
-
-        public static bool TryParseID(this string texto, out int id)
-            => int.TryParse(texto, out id);
-
-        public static bool TryParseID(this string texto, out ulong id)
-            => ulong.TryParse(texto.Replace("#", string.Empty), out id);
-
+     
         public static string Titulo(this string titulo)
             => "⌈" + titulo + "⌋";
 
-        public static string FirstUpper(this string texto)
-            => texto.First().ToString().ToUpper() + texto.Substring(1);
-
         public static string Url(this string texto, string site)
             => Formatter.MaskedUrl($"`{texto}` ", new Uri(site));
-
-        public static string FormatarURLComando(string prefixo, string texto, string hover, string site = "https://discord.gg/zjvb5kUc8q")
-           => Formatter.MaskedUrl($"`{prefixo}{texto}` ", new Uri(site), hover);
     }
 }
