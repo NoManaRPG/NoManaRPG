bdac2b3b4a80e232dccee533f99d95b6ac97c6fc
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-10 15:46:33 -0300

    new command LookAround

--- WafclastRPG/Bot.cs
+++ WafclastRPG/Bot.cs
@@ -12,54 +12,50 @@ using WafclastRPG.Commands.UserCommands.CombatCommands;
 using WafclastRPG.DataBases;
 using WafclastRPG.DiscordEvents;
 
-namespace WafclastRPG
-{
-    public class BotInfo
-    {
-        public int Membros;
-        public int Guildas;
-        public DateTime TempoAtivo { get; set; } = DateTime.Now;
+namespace WafclastRPG {
+  public class BotInfo {
+    public int Membros;
+    public int Guildas;
+    public DateTime TempoAtivo { get; set; } = DateTime.Now;
+  }
+
+  public class Bot {
+    public DiscordClient Client { get; private set; }
+    public CommandsNextExtension CommandsNext { get; private set; }
+
+    public Bot(DiscordConfiguration discordConfiguration)
+        => Client = new DiscordClient(discordConfiguration);
+
+    public Task ConectarAsync() => Client.ConnectAsync();
+
+    public void ModuleCommand(CommandsNextConfiguration ccfg, DataBase database) {
+      CommandsNext = Client.UseCommandsNext(ccfg);
+      CommandsNext.CommandExecuted += CommandExecutedEvent.Event;
+      CommandsNext.CommandErrored += CommandErroredEvent.EventAsync;
+      Client.Ready += ReadyEvent.Event;
+
+      var botInfo = (BotInfo) CommandsNext.Services.GetService(typeof(BotInfo));
+      Client.GuildAvailable += (c, e) => GuildAvailableEvent.Event(c, e, botInfo);
+      Client.GuildMemberAdded += (c, e) => GuildMemberAddedEvent.Event(c, e, botInfo);
+      Client.GuildMemberRemoved += (c, e) => GuildMemberRemovedEvent.Event(c, e, botInfo);
+      Client.MessageCreated += (c, e) => MessageCreatedEvent.Event(c, e, CommandsNext, database);
+      Client.ClientErrored += ClientErroredEvent.Event;
+
+      Client.UseInteractivity(new InteractivityConfiguration {
+        Timeout = TimeSpan.FromSeconds(30),
+        PollBehaviour = PollBehaviour.KeepEmojis,
+        PaginationBehaviour = PaginationBehaviour.Ignore,
+        PaginationDeletion = PaginationDeletion.KeepEmojis,
+      });
+
+      CommandsNext.RegisterCommands<HelpCommand>();
+      CommandsNext.RegisterCommands<InfoCommand>();
+      CommandsNext.RegisterCommands<DatabaseCommands>();
+      CommandsNext.RegisterCommands<AdminCommand>();
+      CommandsNext.RegisterCommands<AttackCommand>();
+      CommandsNext.RegisterCommands<StartCommand>();
+      CommandsNext.RegisterCommands<ExploreCommand>();
+      CommandsNext.RegisterCommands<LookAroundCommand>();
     }
-
-    public class Bot
-    {
-        public DiscordClient Client { get; private set; }
-        public CommandsNextExtension CommandsNext { get; private set; }
-
-        public Bot(DiscordConfiguration discordConfiguration)
-            => Client = new DiscordClient(discordConfiguration);
-
-        public Task ConectarAsync() => Client.ConnectAsync();
-
-        public void ModuleCommand(CommandsNextConfiguration ccfg, DataBase database)
-        {
-            CommandsNext = Client.UseCommandsNext(ccfg);
-            CommandsNext.CommandExecuted += CommandExecutedEvent.Event;
-            CommandsNext.CommandErrored += CommandErroredEvent.EventAsync;
-            Client.Ready += ReadyEvent.Event;
-
-            var botInfo = (BotInfo)CommandsNext.Services.GetService(typeof(BotInfo));
-            Client.GuildAvailable += (c, e) => GuildAvailableEvent.Event(c, e, botInfo);
-            Client.GuildMemberAdded += (c, e) => GuildMemberAddedEvent.Event(c, e, botInfo);
-            Client.GuildMemberRemoved += (c, e) => GuildMemberRemovedEvent.Event(c, e, botInfo);
-            Client.MessageCreated += (c, e) => MessageCreatedEvent.Event(c, e, CommandsNext, database);
-            Client.ClientErrored += ClientErroredEvent.Event;
-
-            Client.UseInteractivity(new InteractivityConfiguration
-            {
-                Timeout = TimeSpan.FromSeconds(30),
-                PollBehaviour = PollBehaviour.KeepEmojis,
-                PaginationBehaviour = PaginationBehaviour.Ignore,
-                PaginationDeletion = PaginationDeletion.KeepEmojis,
-            });
-
-            CommandsNext.RegisterCommands<HelpCommand>();
-            CommandsNext.RegisterCommands<InfoCommand>();
-            CommandsNext.RegisterCommands<DatabaseCommands>();
-            CommandsNext.RegisterCommands<AdminCommand>();
-            CommandsNext.RegisterCommands<AttackCommand>();
-            CommandsNext.RegisterCommands<StartCommand>();
-            CommandsNext.RegisterCommands<ExploreCommand>();
-        }
-    }
+  }
 }
---
+++ WafclastRPG/Commands/UserCommands/LookAroundCommand.cs
@@ -0,0 +1,59 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Text;
+using System.Threading.Tasks;
+using WafclastRPG.Attributes;
+using WafclastRPG.DataBases;
+using WafclastRPG.Entities;
+using WafclastRPG.Entities.Characters;
+using WafclastRPG.Extensions;
+using WafclastRPG.Properties;
+
+namespace WafclastRPG.Commands.UserCommands {
+  public class LookAroundCommand : BaseCommandModule {
+    public DataBase database;
+
+    [Command("olhar")]
+    [Aliases("look", "lookaround", "olhar-em-volta")]
+    [Description("Permite se localizar olhando o terreno em volta.")]
+    [Usage("olhar")]
+    public async Task LookAroundCommandAsync(CommandContext ctx) {
+      await ctx.TriggerTypingAsync();
+      Response response;
+      using (var session = await database.StartDatabaseSessionAsync())
+        response = await session.WithTransactionAsync(async (s, ct) => {
+          var player = await session.FindPlayerAsync(ctx.User);
+          if (player == null)
+            return new Response(Messages.AindaNaoCriouPersonagem);
+
+          var character = player.Character;
+
+          var region = await session.FindRegionAsync(character.Region.Id);
+
+          region.Monsters = character.Region.Monsters;
+          region.MonsterAttackSpeedPoints = character.Region.MonsterAttackSpeedPoints;
+          region.PlayerAttackSpeedPoints = character.Region.PlayerAttackSpeedPoints;
+          region.TotalAttackSpeedPoints = character.Region.TotalAttackSpeedPoints;
+
+          character.Region = region;
+
+          var embed = new DiscordEmbedBuilder();
+          embed.WithColor(DiscordColor.Green);
+          embed.WithTitle($"{region.Name} [{region.Id}]");
+          embed.WithDescription(region.Description);
+
+          var str = new StringBuilder();
+          foreach (var item in region.Exits) {
+            str.AppendLine($"{item.Key} - {item.Value}");
+          }
+
+          embed.AddField("Saidas", str.ToString());
+
+          return new Response(embed);
+        });
+
+      await ctx.ResponderAsync(response.Embed);
+    }
+  }
+}
