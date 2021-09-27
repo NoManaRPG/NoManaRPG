981cbf5d48472675c16ba0359206b4fb6423b482
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-27 10:41:38 -0300

    add invite command

--- WafclastRPG/Commands/InfoCommand.cs
+++ WafclastRPG/Commands/InfoCommand.cs
@@ -20,13 +20,15 @@ namespace WafclastRPG.Commands {
       embed.WithAuthor(ctx.Client.CurrentUser.Username, "https://discord.gg/MAR4NFq", ctx.Client.CurrentUser.AvatarUrl);
       embed.WithThumbnail(ctx.Client.CurrentUser.AvatarUrl);
       embed.WithTitle("RPG para quem usa muito o discord!");
-      var criador = ctx.Client.CurrentApplication.Owners.First();
+      //var criador = ctx.Client.CurrentApplication.Owners.First();
       var str = new StringBuilder();
       str.AppendLine("Servidor Oficial".Url("https://discord.gg/MAR4NFq"));
       str.AppendLine("Servidor Parceiro".Url("https://discord.gg/7x5YkZY"));
       str.AppendLine();
-      str.AppendLine("Vote no bot".Url("https://top.gg/bot/732598033962762402"));
+      str.AppendLine("Vote".Url("https://top.gg/bot/732598033962762402"));
       str.AppendLine("Proximos updates".Url("https://github.com/WafclastRPG/WafclastRPG/issues"));
+      str.AppendLine("Atualização recente".Url("https://discord.gg/d6c6XfGCpU"));
+      str.AppendLine("Anuncios".Url("https://discord.gg/PYZ5Muj2GT"));
       str.AppendLine("Github".Url("https://github.com/WafclastRPG/WafclastRPG"));
       embed.WithDescription(str.ToString());
       embed.AddField("Convite", "Adicione o bot no seu Servidor!".Url("https://discord.com/api/oauth2/authorize?client_id=732598033962762402&permissions=0&scope=bot"));
@@ -34,12 +36,12 @@ namespace WafclastRPG.Commands {
       Process proc = Process.GetCurrentProcess();
       var mem = proc.PrivateMemorySize64;
       embed.AddField("Memoria usada", $"{(mem / 1024) / 1024} Mb", true);
-      var str2 = new StringBuilder();
-    //  str2.Append($"{botInfo.Guildas} guildas");
-    //  str2.Append($" com {botInfo.Membros} membros no total");
-      embed.AddField("Estou em", str2.ToString(), true);
+      // var str2 = new StringBuilder();
+      //  str2.Append($"{botInfo.Guildas} guildas");
+      //  str2.Append($" com {botInfo.Membros} membros no total");
+      //embed.AddField("Estou em", str2.ToString(), true);
       embed.WithColor(DiscordColor.HotPink);
-      embed.WithFooter($"Tempo de resposta do servidor: {ctx.Client.Ping}ms");
+      embed.WithFooter($"Tempo de resposta do bot: {ctx.Client.Ping}ms");
       await ctx.RespondAsync(embed: embed.Build());
     }
   }
