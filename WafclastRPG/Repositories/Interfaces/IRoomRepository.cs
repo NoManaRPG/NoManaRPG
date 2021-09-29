fccc7de37c47d76daf55022eac50707d974a4693
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-28 21:19:47 -0300

    Refactored RoomRepository

--- WafclastRPG/Repositories/Interfaces/IRoomRepository.cs
+++ WafclastRPG/Repositories/Interfaces/IRoomRepository.cs
@@ -3,6 +3,7 @@ using WafclastRPG.Entities.Wafclast;
 
 namespace WafclastRPG.Repositories.Interfaces {
   public interface IRoomRepository {
+    Task<Room> FindRoomOrDefaultAsync(Player player);
     Task<Room> FindRoomOrDefaultAsync(ulong id);
     Task<Room> FindRoomOrDefaultAsync(string name);
     Task SaveRoomAsync(Room item);
--- WafclastRPG/Repositories/RoomRepository.cs
+++ WafclastRPG/Repositories/RoomRepository.cs
@@ -7,17 +7,17 @@ using WafclastRPG.Repositories.Interfaces;
 namespace WafclastRPG.Repositories {
   public class RoomRepository : IRoomRepository {
     private readonly MongoDbContext _context;
+    private readonly FindOptions _options = new FindOptions { Collation = new Collation("pt", false, strength: CollationStrength.Primary) };
 
     public RoomRepository(MongoDbContext context) {
       _context = context;
     }
 
-    public Task<Room> FindRoomOrDefaultAsync(ulong id)
-      => _context.Rooms.Find(x => x.Id == id).FirstOrDefaultAsync();
-    public Task<Room> FindRoomOrDefaultAsync(string name)
-      => _context.Rooms.Find(x => x.Name == name, new FindOptions { Collation = new Collation("pt", false, strength: CollationStrength.Primary) }).FirstOrDefaultAsync();
+    public Task<Room> FindRoomOrDefaultAsync(Player player) => FindRoomOrDefaultAsync(player.Id);
+    public Task<Room> FindRoomOrDefaultAsync(ulong id) => _context.Rooms.Find(x => x.Id == id).FirstOrDefaultAsync();
+    public Task<Room> FindRoomOrDefaultAsync(string name) => _context.Rooms.Find(x => x.Name == name, _options).FirstOrDefaultAsync();
 
-    public Task SaveRoomAsync(Room room)
-      => _context.Rooms.ReplaceOneAsync(x => x.Id == room.Id, room, new ReplaceOptions { IsUpsert = true });
+
+    public Task SaveRoomAsync(Room room) => _context.Rooms.ReplaceOneAsync(x => x.Id == room.Id, room, new ReplaceOptions { IsUpsert = true });
   }
 }
